6294f10c1826ca6ba2ff40a937bcc8326149e929
JackSelf97 <selfbrothers@hotmail.com>
2022-02-25 15:52:15 +0000

    Assets & Parallax Background added

---
+++ RE_E1 - Falmouth University/Assets/Scripts/Parallax_Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax_Script : MonoBehaviour
+{
+    private float length, startPos, yPos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        yPos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float distance = (cam.transform.position.x * parallaxEffect);
+        float ydistance = (cam.transform.position.y * parallaxEffect);
+        transform.position = new Vector3(startPos + distance, yPos + ydistance, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
