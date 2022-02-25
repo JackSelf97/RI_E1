555462d10b68c9df1b4180be7ee3af05aea58bc5
JackSelf97 <selfbrothers@hotmail.com>
2022-02-25 13:37:06 +0000

    Bullet Script added

---
+++ RE_E1 - Falmouth University/Assets/BulletSpawner_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner_Script : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletPos;
+    [SerializeField] private float timeLimt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLimt += Time.deltaTime;
+        if (timeLimt >= 2)
+        {
+            SpawnProjectile();
+            timeLimt = 0;
+        }
+    }
+
+    public void SpawnProjectile()
+    {
+        var bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = bulletPos.position;
+    }
+}
---
+++ RE_E1 - Falmouth University/Assets/Bullet_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Script : MonoBehaviour
+{
+    private Rigidbody2D myRb;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+        myRb.velocity = new Vector2(-speed, myRb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            //GameManager.gMan.TakeDamage();
+        }
+
+        Destroy(gameObject);
+    }
+}
