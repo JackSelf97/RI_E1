12fab9e474f88e72f84c5833c5e86c5902dad792
JackSelf97 <selfbrothers@hotmail.com>
2022-03-01 16:24:09 +0000

    Minor Updates

--- RE_E1 - Falmouth University/Assets/Scripts/GameManager.cs
+++ RE_E1 - Falmouth University/Assets/Scripts/GameManager.cs
@@ -93,9 +93,12 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        if (currentCharacter == null)
         {
-            LoadLastCheckpoint();
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                LoadLastCheckpoint();
+            }
         }
     }
 
